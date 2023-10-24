d95b2b76f795a811a8dff75c7db07fa8cf5b9d9e
beng355826 <benjamin-green@live.co.uk>
2023-10-24 22:29:16 +0100

    fixing enpoints

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,19 +1,22 @@
 let router = require('express').Router()
-const fs = require('fs')
+const fs = require('fs/promises')
 const createUserController = require('../controllers/createUser.controller')
 const getAllUsersController = require('../controllers/getAllUsers.controller')
 const getUserByIdController = require('../controllers/getUserById.controller')
 const updateUserByIdController = require('../controllers/updateUserById.controller')
 const deleteUserByIdController = require('../controllers/deleteUserById.controller')
 
-router.get('/', async (req,res,next) => {
-try{
-    const data = fs.readFileSync(`/Users/bennygreen/Documents/Dtekit-Backend/endpoints.json`, 'utf-8')
-    const parsed = JSON.parse(data)
-   res.json(parsed)
-} catch (err) {
-    next(err);
-}
+router.get('/', (req,res,next) => {
+
+   fs.readFile('/Users/bennygreen/Documents/Dtekit-Backend/endpoints.json', 'utf-8').then((endPoints , err)=> {
+        const parsed = JSON.parse(endPoints)
+        console.log(parsed);
+        res.status(200).json(parsed)  
+    
+    }).catch((err) => {
+        res.status(500).json({fsReadFile: 'failed'})
+    })
+
 })
 
 router.post('/users', createUserController)
