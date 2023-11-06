e6a231de711ba72d21c25488abe101e5cd68e36b
beng355826 <benjamin-green@live.co.uk>
2023-11-06 04:03:32 +0000

    still need to do log in

--- db-cloud/connectCloud.model.js
+++ db-cloud/connectCloud.model.js
@@ -1,12 +1,13 @@
 const Multer = require('multer');
 const {Storage} = require('@google-cloud/storage')
-
+const dotenv = require('dotenv')
+dotenv.config()
 const multer = Multer({
     storage: Multer.memoryStorage(),       
 })
 
 let projectId = 'norse-avatar-403219'             
-let keyFilename = '/Users/bennygreen/Documents/Dtekit-Backend/norse-avatar-403219-8d0df66de38b.json'
+let keyFilename = process.env.CLOUD_STORAGE_KEY
 
 const storage = new Storage({
     projectId,
