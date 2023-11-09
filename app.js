9488e27607ca22e11302507b20126a490840cdd0
beng355826 <benjamin-green@live.co.uk>
2023-11-09 05:47:15 +0000

    enabling cors

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const {error400, error404 ,error500} = require('./error-handling')
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const userRoutes = require('./routes/user-routes')
 const endpointRoute = require('./routes/endpoint-route')
 const picRoutes = require('./routes/pic-routes')
 const getMixesController = require('./MixControllers/getMixes.controller')
+
+app.use(cors())
 
 app.use(express.json())
 
